6286f598ad3883eb60ece7fc69f797efad69008c
Guglielmo Fonda <gufo0125@gmail.com>
2021-05-28 11:47:42 +0400

    Update Button_experiment.ino
    Functioning button pressing (green and red)

--- Button_experiment/Button_experiment.ino
+++ Button_experiment/Button_experiment.ino
@@ -6,9 +6,34 @@ void setup(){
 
 }
 
+  CRGB colors[] = {
+    0xfe0000,
+    0x1ED35E
+  };
+  int p,x,y;
 
 void loop() {
- 
-  delay(100);
 
+  if(M5.Btn.wasPressed()) {
+    p = 1;
+    for(x=0;x<5;x++){
+      for(y=0;y<5;y++){
+        M5.dis.drawpix(x,y,colors[p]);
+        }
+      }
+    delay(100);
+    }
+    else {
+      p = 0;
+      for(x=0;x<5;x++){
+        for(y=0;y<5;y++){
+          M5.dis.drawpix(x,y,colors[p]);
+          }
+        }
+      delay(100);
+      }
+
+ 
+  delay(300);
+  M5.update();
 }
