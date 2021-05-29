10f7a708f6a527b23d8a92940d1c7426418f0b1f
Guglielmo Fonda <gufo0125@gmail.com>
2021-05-29 13:19:58 +0400

    Update Task4_skeleton Delay()
    Delay() added reading presses also while waiting. We don't miss and clicks this way

--- Task_4/Gu/Task_4_skeleton/Task_4_skeleton.ino
+++ Task_4/Gu/Task_4_skeleton/Task_4_skeleton.ino
@@ -1,6 +1,7 @@
 #include "M5Atom.h"
 
 int oldX, oldY;
+bool Delay();
 
 void setup() {
   M5.begin(true, false, true);
@@ -10,7 +11,6 @@ void setup() {
   oldY = -1;
   Serial.println("Initializing M5Stack Atom Matrix");
 }
-
 
 CRGB colors[] = {
   0xfe0000, // red
@@ -23,7 +23,6 @@ int time_blink = 300;
 int x, y, p;
 int count = 0;
 bool Blink = 0;
-
 
 void loop() {
 
@@ -70,15 +69,18 @@ void loop() {
     //count exceeded possible click options
     default:
       count = 0;
-
   }
 
   Serial.println(count);
 
   if (Blink) {
-    delay(time_blink);
+    if(Delay(time_blink)){
+      count++;
+      }
     M5.dis.clear();
-    delay(time_blink-200);
+    if(Delay(time_blink - 200)){
+      count++;
+      }
     Blink = 0;
   }
   else {
@@ -86,7 +88,28 @@ void loop() {
   }
 
   M5.update();
-  delay(200);
+  if(Delay(200)){
+    count++;
+    }
+}
 
 
+bool Delay(int num) {
+  bool check_pressed = 0;
+  int time_passed = 0;
+
+  while (check_pressed != 1 && time_passed < num) {
+    if (M5.Btn.wasPressed()) {
+      check_pressed = 1;
+      
+    }
+    delay(10);
+    time_passed = time_passed + 10;
+    Serial.println("This is the time passed so far");
+    Serial.println(time_passed);
+    Serial.println("This is the check");
+    Serial.println(check_pressed);
+    M5.update();
+  }
+  return check_pressed;
 }
