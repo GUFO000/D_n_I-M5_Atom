7f4e12adc8868eb01982fc29adf5fb548890107b
Guglielmo Fonda <gufo0125@gmail.com>
2021-05-30 23:32:56 +0400

    Update Gyroscope.ino
    Update till tonight. Working on the sampling freq

--- Part 1_second/Gu/Gyroscope/Gyroscope.ino
+++ Part 1_second/Gu/Gyroscope/Gyroscope.ino
@@ -14,8 +14,8 @@ CRGB colors[] = {
 float accX, accY, accZ;
 float gyroX, gyroY, gyroZ;
 float ahrsX_pitch, ahrsY_roll, ahrsZ_yaw;
-
 float temp;
+
 int time_blink = 300;
 int x, y, p;
 int count = 0;
@@ -25,28 +25,35 @@ int state_on_off  = 0;
 unsigned long timeSinceLastScreenRefresh;
 unsigned long timeBtwScreenRefresh = 200;
 
+unsigned long timeSinceLastChipRefresh;
+unsigned long timeBtwChipRefresh = 10000;
+
 unsigned long time_milli;
 
 void loop() {
 
   time_milli = millis();
 
-  M5.IMU.getTempData(&temp);
-  M5.IMU.getGyroData(&gyroX, &gyroY, &gyroZ);
-
-  //Serial.println("Coordinates");
-  //  Serial.println(gyroX);
-  // Serial.println(gyroY);
-  //  Serial.println(gyroZ);
 
 
-  M5.IMU.getAhrsData(&ahrsX_pitch, &ahrsY_roll, &ahrsZ_yaw);
-  Serial.print("ahrsX: ");
-  Serial.print(ahrsX_pitch);
-  Serial.print(", ahrsY: ");
-  Serial.print(ahrsY_roll);
-  Serial.print(", ahrsZ: ");
-  Serial.println(ahrsZ_yaw);
+
+  switch (state_on_off) {
+
+    //mode 0 is off
+    case 0:
+
+      break;
+
+    //mode 1 is on choosing mode
+    case 1:
+      break;
+
+    //mode 2, one mode is selected
+    case 2:
+      break;
+
+
+  }
 
 
 
@@ -54,7 +61,7 @@ void loop() {
     if (state_on_off == 0) {
 
       state_on_off = 1;
-      p=1;
+      p = 1;
       for (x = 0; x < 5; x++) {
         for (y = 0; y < 5; y++) {
           M5.dis.drawpix(x, y, colors[p]);
@@ -67,6 +74,29 @@ void loop() {
     }
 
     M5.update();
+    timeSinceLastScreenRefresh = time_milli;
+  }
+
+
+  if (time_milli - timeBtwChipRefresh > timeSinceLastChipRefresh) {
+    
+    M5.IMU.getTempData(&temp);
+    M5.IMU.getGyroData(&gyroX, &gyroY, &gyroZ);
+    M5.IMU.getAccelData(&accX, &accY, &accZ);
+    M5.IMU.getAhrsData(&ahrsX_pitch, &ahrsY_roll, &ahrsZ_yaw);
+
+    // Serial.println("Coordinates");
+    // Serial.println(gyroX);
+    // Serial.println(gyroY);
+    // Serial.println(gyroZ);
+
+      Serial.print("ahrsX: ");
+      Serial.print(ahrsX_pitch);
+      Serial.print(", ahrsY: ");
+      Serial.print(ahrsY_roll);
+      Serial.print(", ahrsZ: ");
+      Serial.println(ahrsZ_yaw);
+    
     timeSinceLastScreenRefresh = time_milli;
   }
 }
