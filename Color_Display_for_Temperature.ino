508973917a891290787e262dec493675e9013726
aliha00 <aan440@nyu.edu>
2021-05-30 23:24:23 +0400

    Create Color_Display_for_Temperature.ino

---
+++ Color_Display_for_Temperature.ino
@@ -0,0 +1,59 @@
+#include <M5Atom.h>
+
+ int a,b;
+
+ void setup() {
+   M5.begin(true, false, true);
+   Serial.begin(115200);
+ }
+
+
+ float temp;
+
+ void loop() {
+  
+  M5.IMU.getTempData(&temp);
+  Serial.println(temp);
+//temp = 50;
+
+ if (temp <=15){ // <= 15
+    for (a=0;a<5;a++)
+    {
+      for (b = 0 ; b < 5;b++) {
+        M5.dis.drawpix(a,b, 0xff0080);
+      }
+    }
+  }
+  else if (temp > 15 && temp <=30 ) // 15 < temp <= 30
+   {
+     for (a=0;a<5;a++)
+     {
+       for (b = 0 ; b < 5;b++)
+       {
+         M5.dis.drawpix (a,b,0x1ED35E );
+       }
+     }
+   }
+   else if (temp > 30 && temp <= 45 ) // 30 < temp <= 45
+   {
+     for (a=0;a<5;a++)
+     {
+       for (b = 0 ; b < 5;b++)
+       {
+         M5.dis.drawpix (a,b,0x00ff00  );
+       }
+     }
+   }
+   else if (temp > 45 ) // temp > 45
+   {
+     for (a=0;a<5;a++)
+     {
+       for (b = 0 ; b < 5;b++)
+       {
+         M5.dis.drawpix (a,b, 0xff0000 );
+       }
+     }
+   }
+   delay(100);
+ M5.update();
+ }
