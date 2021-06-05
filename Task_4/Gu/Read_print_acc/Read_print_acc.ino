9473790aef0dba22a10a1d5728351d498fe3c663
Guglielmo Fonda <gufo0125@gmail.com>
2021-06-05 13:32:17 +0400

    Update Read_print_acc.ino

--- Task_4/Gu/Read_print_acc/Read_print_acc.ino
+++ Task_4/Gu/Read_print_acc/Read_print_acc.ino
@@ -54,15 +54,15 @@ void loop() {
     }
   }
 
-L
-  Serial.println("This was the new X acc ");
-  Serial.println(accZ);
+
+  Serial.print("This was the new X acc ");
+  Serial.print(accX);
+  Serial.print(" This was the new Y acc ");
+  Serial.println(accY);
   //Serial.println("This was the new Y position ");
   //Serial.println(accY);
 
   M5.dis.clear();
-  
-  delay(2);
-  
+    
 
 }
