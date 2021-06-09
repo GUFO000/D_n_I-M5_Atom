a3a026ad5d5a8ae346e77775e7958106543a7b91
Guglielmo Fonda <gufo0125@gmail.com>
2021-06-10 03:13:43 +0400

    Fixed 3 out of 4 modes
    Still working on the graph

--- Code_for_submission/M5_part1_second_mode_changing_exit_enter/M5_part1_second_mode_changing_exit_enter.ino
+++ Code_for_submission/M5_part1_second_mode_changing_exit_enter/M5_part1_second_mode_changing_exit_enter.ino
@@ -1,41 +1,104 @@
 #include "M5Atom.h"
 //#include <FastLED.h>
+//#define NUM_LEDS 25
+//#define DATA_PIN 27
+
+int n, mat;
+char string_temp[10];
 
 int zero[25] = {
   0, 1, 1, 1, 0,
   0, 1, 0, 1, 0,
   0, 1, 0, 1, 0,
   0, 1, 0, 1, 0,
-  0, 1, 1, 1, 0};
+  0, 1, 1, 1, 0
+};
 
 int one[25] = {
   0, 0, 1, 0, 0,
   0, 1, 1, 0, 0,
   0, 0, 1, 0, 0,
   0, 0, 1, 0, 0,
-  0, 1, 1, 1, 0};
+  0, 1, 1, 1, 0
+};
 
 int two[25] = {
   0, 1, 1, 1, 0,
   0, 0, 0, 1, 0,
   0, 1, 1, 1, 0,
   0, 1, 0, 0, 0,
-  0, 1, 1, 1, 0};
+  0, 1, 1, 1, 0
+};
 
 int tre[25] = {
   0, 1, 1, 1, 0,
   0, 0, 0, 1, 0,
   0, 1, 1, 1, 0,
   0, 0, 0, 1, 0,
-  0, 1, 1, 1, 0};
+  0, 1, 1, 1, 0
+};
 
 int four[25] = {
   0, 1, 0, 1, 0,
   0, 1, 0, 1, 0,
   0, 1, 1, 1, 0,
   0, 0, 0, 1, 0,
-  0, 0, 0, 1, 0};
+  0, 0, 0, 1, 0
+};
 
+int five[25] = {
+  0, 1, 1, 1, 0,
+  0, 1, 0, 0, 0,
+  0, 1, 1, 1, 0,
+  0, 0, 0, 1, 0,
+  0, 1, 1, 1, 0
+};
+
+int six[25] = {
+  0, 1, 0, 0, 0,
+  0, 1, 0, 0, 0,
+  0, 1, 1, 1, 0,
+  0, 1, 0, 1, 0,
+  0, 1, 1, 1, 0
+};
+
+int seven[25] = {
+  0, 1, 1, 1, 0,
+  0, 0, 0, 1, 0,
+  0, 0, 0, 1, 0,
+  0, 0, 0, 1, 0,
+  0, 0, 0, 1, 0
+};
+
+int eight[25] = {
+  0, 1, 1, 1, 0,
+  0, 1, 0, 1, 0,
+  0, 1, 1, 1, 0,
+  0, 1, 0, 1, 0,
+  0, 1, 1, 1, 0
+};
+
+int nine[25] = {
+  0, 1, 1, 1, 0,
+  0, 1, 0, 1, 0,
+  0, 1, 1, 1, 0,
+  0, 0, 0, 1, 0,
+  0, 0, 0, 1, 0
+};
+int letter_c[25] = {
+  0, 1, 1, 1, 0,
+  1, 1, 0, 0, 0,
+  1, 0, 0, 0, 0,
+  1, 0, 0, 0, 0,
+  0, 1, 1, 1, 0
+};
+int letter_f[25] = {
+  0, 1, 1, 1, 0,
+  0, 1, 0, 0, 0,
+  0, 1, 1, 0, 0,
+  0, 1, 0, 0, 0,
+  0, 1, 0, 0, 0
+};
 //---------------------------------------
 
 bool face_down();
@@ -44,12 +107,23 @@ int check_mode_switch();
 bool enter_exit_mode();
 void DisplayColor(int number[], int fillColor);
 
+void update_temp_average();
+
+void print_digit(char digit);
+
 
 //Few colors available
 CRGB colors[] = {
   0xfe0000, // red
   0x1ED35E, // green
-  0xFFFFFF // white
+  0xFFFFFF, // white
+  0x031cfc, // blue
+  0x2090f7, //light blue
+  0x137a10, //green
+  0xc76316, // orange
+  0xc71616, //red
+  
+  
 };
 
 
@@ -57,7 +131,7 @@ CRGB colors[] = {
 //physical variables that the M5 can read
 float accX, accY, accZ;
 float ahrsX_pitch, ahrsY_roll, ahrsZ_yaw;
-float temp;
+float temp, tempf;
 
 //variables to identify LED on the matrix
 int x, y;
@@ -74,7 +148,7 @@ int store_mode =  0;
 int Blink = 0;
 
 //threshold for inclining the matrix
-int thold = 17; //degrees 
+int thold = 17; //degrees
 float acc_thold = 1.2; //g (m*s^2)
 
 //Defining refresh times
@@ -108,14 +182,25 @@ unsigned long timeSinceLastModeButtonPress = 0;
 unsigned long timeBtwLastModeButtonPress = 100;
 unsigned long time_milli_lastButtonPress = 0;
 
+unsigned long time_btw_digitUpdate_mode0 = 600;
+unsigned long time_last_digitUpdate_mode0 = 0;
+
+
+
 bool MODE_DETECTING = false;
 bool MODE_SET = false;
+
+float temp_average = 0;
+float temp_number_entries = 0;
 
 //-----------------------------------------------------------------
 
 void setup() {
-  M5.begin(true, true, true);
+  M5.begin(true, false, true);
   M5.IMU.Init();
+
+  //  FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS);
+  //  FastLED.setBrightness(20);
 }
 
 //-----------------------------------------------------------------
@@ -152,7 +237,7 @@ void loop() {
   if (face_is_up && MODE_DETECTING) {
 
     store_mode  = check_mode_switch();
-    
+
     //    Serial.print("check_mode_switch() ");
     //    Serial.println(store_mode);
 
@@ -172,11 +257,15 @@ void loop() {
       //Show Active temperature + Units
       case 0:
         DisplayColor(zero, 1);
+        n = 0;
+        dtostrf(temp, 5, 2, string_temp);
         break;
 
       //Show average of last 24 hours of temperature + Units
       case 1:
         DisplayColor(one, 1);
+        n = 0;
+        dtostrf(temp_average, 5, 2, string_temp);
         break;
 
       //Show color scale of temperature range + current temperature as color
@@ -195,48 +284,114 @@ void loop() {
         break;
     }
 
-    if(enter_exit_mode()){
-        MODE_DETECTING = false;
-        MODE_SET = true;
-      }    
+    if (enter_exit_mode()) {
+      MODE_DETECTING = false;
+      MODE_SET = true;
     }
+  }
 
-    //in this if we will execute the different modes
-    if (face_is_up && MODE_SET){
-      switch(active_mode){
-         case 0:
-          DisplayColor(zero, 2);
-          break; 
-         case 1:
-          DisplayColor(one, 2);
-          break;
-         case 2:
-          DisplayColor(two, 2);
-          break;
-         case 3:
-          DisplayColor(tre, 2);
-          break;
-         case 4:
-          DisplayColor(four, 2);
-          break; 
-      }
-      if(enter_exit_mode()){
-        MODE_DETECTING = true;
-        MODE_SET = false;
-      } 
+  //in this if we will execute the different modes
+  if (face_is_up && MODE_SET) {
+    switch (active_mode) {
+      case 0:
+        dtostrf(temp, 5, 2, string_temp);
+        /*//        if (time_milli - time_last_update_mode0 > time_btw_update_mode0) {
+          //          string_temp[10] = ' ';
+          //          Serial.print(temp);
+          //
+          //          dtostrf(temp, 5, 2, string_temp);
+          //          Serial.print("string tmp---------");
+          //          Serial.println(string_temp);
+          //          time_last_update_mode0 = time_milli;
+          //        }*/
+
+        if (time_milli - time_last_digitUpdate_mode0 > time_btw_digitUpdate_mode0) {
+          Serial.print("---------string tmp");
+          Serial.println(string_temp);
+          if (n == strlen(string_temp)) {
+            n = 0;
+            DisplayColor(letter_c, 1);
+          }
+          else {
+            print_digit(string_temp[n]);
+            n++;
+          }
+          time_last_digitUpdate_mode0 = time_milli;
+        }
+        break;
+      case 1:
+        //DisplayColor(one, 2);
+        dtostrf(temp_average, 5, 2, string_temp);
+        if (time_milli - time_last_digitUpdate_mode0 > time_btw_digitUpdate_mode0) {
+
+          if (n == strlen(string_temp)) {
+            n = 0;
+            DisplayColor(letter_c, 1);
+          }
+          else {
+            print_digit(string_temp[n]);
+            n++;
+          }
+          time_last_digitUpdate_mode0 = time_milli;
+        }
+        break;
+      case 2:
+        M5.dis.clear();
+        M5.dis.drawpix(0, 4, colors[3] );
+        M5.dis.drawpix(1, 4, colors[4] );
+        M5.dis.drawpix(2, 4, colors[5] );
+        M5.dis.drawpix(3, 4, colors[6] );
+        M5.dis.drawpix(4, 4, colors[7] );
+
+        if (temp > 40){mat  = 7;}
+        else if (temp > 35){mat  = 6;}
+        else if (temp > 30){mat  = 5;}
+        else if (temp > 25){mat  = 4;}
+        else if (temp > 20){mat  = 3;}
+        
+        for (x = 0; x < 5; x++) {
+          for (y = 0; y < 3; y++) {
+            M5.dis.drawpix(x, y, colors[mat]);
+          }
+        }
+
+        break;
+      case 3:
+
+        
+        DisplayColor(tre, 2);
+        break;
+      case 4:
+        tempf = (temp * (9/5))+32;
+        dtostrf(tempf, 5, 2, string_temp);
+        Serial.println(tempf);
+        if (time_milli - time_last_digitUpdate_mode0 > time_btw_digitUpdate_mode0) {
+          if (n == strlen(string_temp)) {
+            n = 0;
+            DisplayColor(letter_f, 1);
+          }
+          else {
+            print_digit(string_temp[n]);
+            n++;
+          }
+          time_last_digitUpdate_mode0 = time_milli;
+        }
+        break;
     }
-
-/*  //Refresh screen after equal intervals
-  if (time_milli - timeSinceLastScreenRefresh > timeBtwScreenRefresh) {
-    M5.update();
-    timeSinceLastScreenRefresh = time_milli;
-    //Serial.println("Screen updated----------------");
-  }*/
+    if (enter_exit_mode()) {
+      MODE_DETECTING = true;
+      MODE_SET = false;
+    }
+  }
 
   //Refresh chip in equal intervals
   if (time_milli - timeSinceLastChipRefresh > timeBtwChipRefresh) {
 
     M5.IMU.getTempData(&temp);
+    Serial.print("Updating stuff_______-_-_-_-_-_-_");
+    Serial.println(temp);
+    update_temp_average();
+
     M5.IMU.getAccelData(&accX, &accY, &accZ);
     M5.IMU.getAhrsData(&ahrsX_pitch, &ahrsY_roll, &ahrsZ_yaw);
 
@@ -287,7 +442,7 @@ bool face_up() {
 
 //-----------------------------------------------------------------
 
-bool enter_exit_mode(){
+bool enter_exit_mode() {
   bool check = 0;
   M5.update();
   if (M5.Btn.wasPressed()) {
@@ -306,22 +461,9 @@ int check_mode_switch() {
   int mode_changed = 0;
   float magnitude_acc = 0;
 
-//  Serial.print("The magnitude of the X acceleration is: ");
-//  Serial.println(accX);
+  //  Serial.print("The magnitude of the X acceleration is: ");
+  //  Serial.println(accX);
 
-/*  if (M5.Btn.wasPressed()) {
-    time_milli_press = millis();
-
-    if (time_milli_press - time_milli_last_press > time_btw_btn_press) {
-      //        Serial.print("Tempo passato ultimo tocco ");
-      //        Serial.println((time_milli_press - time_milli_last_press));
-
-      time_milli_last_press  = time_milli_press;
-      mode_changed = 1;
-    }
-  }
-  else*/ 
-  
   if (((abs(accX) > 0.37) || (abs(accY) > 0.37)) || (abs(accX) > 0.37) && (abs(accY) > 0.37)) {
     time_milli_press = millis();
 
@@ -341,12 +483,57 @@ int check_mode_switch() {
   return mode_changed;
 }
 
-void DisplayColor(int number[], int fillColor){
-    M5.dis.clear();
-    for (int i = 0; i < 25; i++)
-    {   
-        if(number[i] == 1){
-        M5.dis.drawpix(i, colors[fillColor]);
-        }
+void DisplayColor(int number[], int fillColor) {
+  M5.dis.clear();
+  for (int i = 0; i < 25; i++)
+  {
+    if (number[i] == 1) {
+      M5.dis.drawpix(i, colors[fillColor]);
     }
+  }
+}
+
+void update_temp_average() {
+  temp_number_entries++;
+  temp_average = (temp_number_entries - 1) / temp_number_entries * temp_average + temp / temp_number_entries;
+
+}
+
+
+
+void print_digit(char digit) {
+  if (digit == '0') {
+    DisplayColor(zero, 1);
+  }
+  else if (digit == '1') {
+    DisplayColor(one, 1);
+  }
+  else if (digit == '2') {
+    DisplayColor(two, 1);
+  }
+  else if (digit == '3') {
+    DisplayColor(tre, 1);
+  }
+  else if (digit == '4') {
+    DisplayColor(four, 1);
+  }
+  else if (digit == '5') {
+    DisplayColor(five, 1);
+  }
+  else if (digit == '6') {
+    DisplayColor(six, 1);
+  }
+  else if (digit == '7') {
+    DisplayColor(seven, 1);
+  }
+  else if (digit == '8') {
+    DisplayColor(eight, 1);
+  }
+  else if (digit == '9') {
+    DisplayColor(nine, 1);
+  }
+  else if (digit == '.') {
+    M5.dis.clear();
+    M5.dis.drawpix(22, colors[1] );
+  }
 }
